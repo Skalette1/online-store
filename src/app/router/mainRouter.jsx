334c6a6417d7caf593e79e58fb9515ab682bc872
Beslan <beslandudaev8@gmail.com>
2025-02-26 22:21:51 +0300

    устранитл ошибку

--- src/app/router/mainRouter.jsx
+++ src/app/router/mainRouter.jsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import { ProductPage } from "../../pages/ui/productPage";
 import { Home } from "../../pages/ui/Home";
 import { FoodPage } from "../../pages/ui/FoodPage";
 import { IzbaPage } from "../../pages/ui/IzbaPage";
 import { Cart } from "../../pages/model/Cart";
+import { ProductPage } from "../../pages/ui/ProductPage.jsx";
 
 export const router = createBrowserRouter([
   {
